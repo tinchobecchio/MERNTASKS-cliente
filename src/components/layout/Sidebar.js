586720b4f378a2d9f02efcb41d3351343010ab1e
Martin Becchio <tincho.becchio@gmail.com>
2023-04-10 02:57:44 -0300

    primeros pasos en proyectos

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,15 @@
+
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <h1>MERN<span>Tasks</span></h1>
+
+            <div className="proyectos">
+                <h2>Tus Proyectos</h2>
+            </div>
+        </aside>
+    );
+}
+ 
+export default Sidebar;
