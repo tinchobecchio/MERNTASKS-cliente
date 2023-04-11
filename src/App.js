6dab5d723e7e0915faf1268a59c0761432774aae
Martin Becchio <tincho.becchio@gmail.com>
2023-04-11 00:34:31 -0300

    creando el provider de proyectos

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/auth/Login';
 import NuevaCuenta from './components/auth/NuevaCuenta';
 import Proyectos from './components/proyectos/Proyectos';
+import ProyectoState from './context/proyectos/proyectoState'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/nueva-cuenta' element={<NuevaCuenta/>} />
-        <Route path='/proyectos' element={<Proyectos />} />
-      </Routes>
-    </Router>
+    <ProyectoState>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/nueva-cuenta' element={<NuevaCuenta/>} />
+          <Route path='/proyectos' element={<Proyectos />} />
+        </Routes>
+      </Router>
+    </ProyectoState>
   );
 }
 
