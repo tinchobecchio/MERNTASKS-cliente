6f861478bdbb11ea5c2d883d44d95e24c677676e
Martin Becchio <tincho.becchio@gmail.com>
2023-04-10 23:00:41 -0300

    states y funciones para nuevos proyectos

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,37 @@
-
+import { useState } from 'react'
 
 const NuevoProyecto = () => {
+
+
+    // State para Proyecto
+    const [ proyecto, guardarProyecto ] = useState({
+        nombre: ''
+    })
+    // Extraer nombre de proyecto
+    const { nombre } = proyecto
+
+    // Lee los contenidos del input
+    const onChangeProyecto = e => {
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Cuando el usuario agrega un proyecto
+    const onSubmitProyecto = e => {
+        e.preventDefault()
+
+        // validar el proyecto
+
+
+        // agregar al state
+
+
+        // reiniciar el form
+    }
+
+
     return (
         <>
             <button
@@ -10,12 +41,15 @@ const NuevoProyecto = () => {
         
             <form 
                 className='formulario-nuevo-proyecto'
+                onSubmit={onSubmitProyecto}
             >
                 <input
                     type="text" 
                     className='input-text'
                     placeholder='Nombre Proyecto'
                     name='nombre'
+                    value={nombre}
+                    onChange={onChangeProyecto}
                 />
 
                 <input 
