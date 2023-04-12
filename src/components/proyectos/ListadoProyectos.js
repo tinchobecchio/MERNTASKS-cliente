59b91aa56e384274e5defe30ec805db773d8185e
Martin Becchio <tincho.becchio@gmail.com>
2023-04-12 18:31:25 -0300

    agregando animacion a los proyectos. tuve que sacar el react strict mode

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import Proyecto from "./Proyecto";
 import proyectoContext from "../../context/proyectos/proyectoContext";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const ListadoProyectos = () => {
 
@@ -18,15 +19,23 @@ const ListadoProyectos = () => {
     if(proyectos.length === 0) return <p>No hay proyectos, comienza creando uno</p>
 
 
-
     return (
         <ul className="listado-proyectos">
-            {proyectos.map(proyecto =>(
-                <Proyecto 
-                key={proyecto.id}
-                    proyecto={proyecto}
-                />
-            ))}
+            <TransitionGroup>
+                {proyectos.map(proyecto =>(
+                    <CSSTransition
+                        key={proyecto.id}
+                        timeout={200}
+                        classNames="proyecto"
+                    >
+                    <Proyecto 
+                        proyecto={proyecto}
+                    />
+                </CSSTransition>
+                ))}
+
+
+            </TransitionGroup>
         </ul>
     );
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+// comente esto para sacarle el warning del findDom cuando le puse las animaciones de react-transition-group
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
