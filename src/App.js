d0e84f749cc2f62cc7ee025e615a02e90414508c
Martin Becchio <tincho.becchio@gmail.com>
2023-04-28 03:41:13 -0300

    creando el state y provider de auth

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Proyectos from './components/proyectos/Proyectos';
 import ProyectoState from './context/proyectos/proyectoState'
 import TareaState from './context/tareas/tareaState';
 import AlertaState from './context/alertas/alertaState';
+import AuthState from './context/autenticacion/authState';
 
 function App() {
   return (
     <ProyectoState>
       <TareaState>
         <AlertaState>
-          <Router>
-            <Routes>
-              <Route path='/' element={<Login />} />
-              <Route path='/nueva-cuenta' element={<NuevaCuenta/>} />
-              <Route path='/proyectos' element={<Proyectos />} />
-            </Routes>
-          </Router>
+          <AuthState>
+            <Router>
+              <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/nueva-cuenta' element={<NuevaCuenta/>} />
+                <Route path='/proyectos' element={<Proyectos />} />
+              </Routes>
+            </Router>
+          </AuthState>
         </AlertaState>
       </TareaState>
     </ProyectoState>
---
+++ src/context/autenticacion/authReducer.js
@@ -0,0 +1,19 @@
+import { 
+    REGISTRO_EXITOSO,
+    REGISTRO_ERROR,
+    OBTENER_USUARIO,
+    LOGIN_EXITOSO,
+    LOGIN_ERROR,
+    CERRAR_SESION
+ } from '../../types/index.js'
+
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+}
+
+export default reducer
